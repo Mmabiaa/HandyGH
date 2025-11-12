814bc0e163c2ef1a995c483b628258265e577b58
Boateng Prince Agyenim <isbbydior@gmail.com>
2025-11-12 14:52:51 +0000

    create the core utilities and middleware

---
+++ backend/core/__init__.py
@@ -0,0 +1,11 @@
+"""
+Core utilities and shared components for HandyGH.
+
+This package contains:
+- Custom exceptions
+- Custom permissions
+- Middleware
+- Validators
+- Utility functions
+- Pagination classes
+"""
